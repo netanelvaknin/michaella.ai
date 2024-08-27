738ecfe41cb6282ff331a3a1fac79162978e47e8
Netanel Vaknin <emailofnetanel@gmail.com>
2024-08-28 01:17:02 +0300

    update deps and route handler

--- src/app/api/upload-file/route.ts
+++ src/app/api/upload-file/route.ts
@@ -1,10 +1,7 @@
-import path from "path";
-import fs from "fs";
 import { GoogleAIFileManager } from "@google/generative-ai/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { prompt } from "@/app/api/upload-file/constants";
 
-const UPLOAD_DIR = path.resolve("uploads");
 const fileManager = new GoogleAIFileManager(process.env.GOOGLE_API_KEY || "");
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || "");
 const model = genAI.getGenerativeModel({
@@ -14,21 +11,12 @@ const model = genAI.getGenerativeModel({
 export async function POST(request: Request) {
   try {
     const formData = await request.formData();
-    const body = Object.fromEntries(formData);
-    const file = (body.files as Blob) || null;
-    const buffer = Buffer.from(await file.arrayBuffer());
-    const filePath = path.resolve(UPLOAD_DIR, (file as File).name);
 
-    if (!fs.existsSync(UPLOAD_DIR)) {
-      fs.mkdirSync(UPLOAD_DIR);
-    }
-
-    fs.writeFileSync(filePath, buffer);
-
-    const uploadResponse = await fileManager.uploadFile(filePath, {
-      mimeType: "application/pdf",
-      displayName: "Gemini 1.5 PDF",
-    });
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/upload/v1beta/files?&key=${fileManager.apiKey}`,
+      { method: "post", body: formData }
+    );
+    const uploadResponse = await response.json();
 
     const result = await model.generateContent([
       {
@@ -41,7 +29,6 @@ export async function POST(request: Request) {
     ]);
 
     const quiz = result.response.text();
-    fs.unlinkSync(filePath);
 
     return Response.json({ res: quiz });
   } catch (e) {
