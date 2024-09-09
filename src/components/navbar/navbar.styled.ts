c3a2d85681c2bb8ca2d453bfb6bdce004e8cf8e5
Netanel Vaknin <emailofnetanel@gmail.com>
2024-09-09 09:41:23 +0300

    add z-index

--- src/components/navbar/navbar.styled.ts
+++ src/components/navbar/navbar.styled.ts
@@ -10,6 +10,7 @@ export const StyledNavbar = styled("nav")`
   align-items: center;
   background: rgba(255, 255, 255, 0.6);
   position: fixed;
+  z-index: 1;
   width: 100%;
   backdrop-filter: blur(8px);
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
